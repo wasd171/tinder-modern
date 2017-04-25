92f763d7c95b1d15ac83f4b36d865c12f330cea4
Konstantin Nesterov <wasd171@gmail.com>
2017-04-25 22:02:25 +0200

    Implemented authorize

--- dist/bundle.es.js
+++ dist/bundle.es.js
@@ -1,5 +1,77 @@
 import 'isomorphic-fetch';
 
-class TinderClient {}
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+const TINDER_HOST = 'https://api.gotinder.com/';
+class TinderClient {
+    constructor() {
+        this.xAuthToken = null;
+        this.lastActivity = new Date();
+        this.userId = null;
+        this.defaults = null;
+    }
+
+    get requestHeaders() {
+        const headers = {
+            'User-Agent': 'Tinder Android Version 4.5.5',
+            'os_version': '23',
+            'platform': 'android',
+            'app-version': '854',
+            'Accept-Language': 'en',
+            'Content-Type': 'application/json'
+        };
+
+        if (this.xAuthToken !== null) {
+            headers['X-Auth-Token'] = this.xAuthToken;
+        }
+
+        return new Headers(headers);
+    }
+
+    http({ path, method, data }) {
+        var _this = this;
+
+        return _asyncToGenerator(function* () {
+            const res = yield fetch(`${TINDER_HOST}${path}`, {
+                method,
+                headers: _this.requestHeaders,
+                body: JSON.stringify(data)
+            });
+
+            console.log({ res });
+
+            if (!res.ok) {
+                const body = yield res.text();
+                throw new Error(body);
+            } else {
+                return res.json();
+            }
+        })();
+    }
+
+    authorize({ fbToken, fbId }) {
+        var _this2 = this;
+
+        return _asyncToGenerator(function* () {
+            const res = yield _this2.http({
+                path: 'auth',
+                method: 'POST',
+                data: {
+                    facebook_token: fbToken,
+                    facebook_id: fbId,
+                    locale: 'en'
+                }
+            });
+
+            console.log({ res });
+
+            _this2.xAuthToken = res.token;
+            _this2.userId = res.user._id;
+            _this2.defaults = res;
+
+            return res;
+        })();
+    }
+}
 
 export default TinderClient;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,66 @@
 import 'isomorphic-fetch'
 
 
-class TinderClient {
+const TINDER_HOST = 'https://api.gotinder.com/';
+const TINDER_IMAGE_HOST = "https://imageupload.gotinder.com/";
 
+class TinderClient {
+    xAuthToken = null;
+    lastActivity = new Date();
+    userId = null;
+    defaults = null;
+
+    get requestHeaders() {
+        const headers = {
+            'User-Agent': 'Tinder Android Version 4.5.5',
+            'os_version': '23',
+            'platform': 'android',
+            'app-version': '854',
+            'Accept-Language': 'en',
+            'Content-Type': 'application/json'    
+        };
+
+        if (this.xAuthToken !== null) {
+            headers['X-Auth-Token'] = this.xAuthToken;
+        }
+
+        return new Headers(headers);
+    }
+
+    async http({path, method, data}) {
+        const res = await fetch(`${TINDER_HOST}${path}`, {
+            method,
+            headers: this.requestHeaders,
+            body: JSON.stringify(data)
+        });
+        
+        if (!res.ok) {
+            const body = await res.text();
+            throw new Error(body);
+        } else {
+            return res.json();
+        }
+    }
+
+    async authorize({fbToken, fbId}) {
+        const res = await this.http({
+            path: 'auth',
+            method: 'POST',
+            data: {
+                facebook_token: fbToken,
+                facebook_id: fbId,
+                locale: 'en'
+            }
+        });
+
+        console.log({res});
+
+        this.xAuthToken = res.token;
+        this.userId = res.user._id;
+        this.defaults = res;
+
+        return res
+    }
 }
 
 export default TinderClient
