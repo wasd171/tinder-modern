d2269fc5ab89e18d40172edc4aca387e31fc5fe0
Konstantin Nesterov <wasd171@gmail.com>
2017-06-23 21:20:55 +0100

    Removed rollup config

--- rollup-configs/cjs.js
+++
@@ -1,8 +0,0 @@
-import shared from './shared'
-
-const config = Object.assign({}, shared, {
-	format: 'cjs',
-	dest: 'dist/bundle.js'
-})
-
-export default config
--- rollup-configs/es.js
+++
@@ -1,8 +0,0 @@
-import shared from './shared'
-
-const config = Object.assign({}, shared, {
-	format: 'es',
-	dest: 'dist/bundle.es.js'
-})
-
-export default config
